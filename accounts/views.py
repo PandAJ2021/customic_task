156caa9be91b46fd40421997456b7bace1b1c9ec
AliJalili <pandaj200021@gmail.com>
2025-11-02 19:16:47 +0330

    feat(auth): add user logout API with JWT token blacklisting

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from .serializers import RegistrationSerializer
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserRegisterView(APIView):
@@ -22,3 +23,20 @@ class UserRegisterView(APIView):
             }, status=status.HTTP_201_CREATED)
         
         return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated,]
+
+    def post(self, request):
+
+        refresh = request.data.get('refresh')
+        if not refresh:
+            return Response({'error':'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            token = RefreshToken(refresh)
+            token.blacklist()
+            return Response({'detail':'Logged out successful'}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as err:
+            return Response({'error':str(err)}, status=status.HTTP_400_BAD_REQUEST)
