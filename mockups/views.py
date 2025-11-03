f1f1970152e4f39acd036e792cde11cec481b429
AliJalili <pandaj200021@gmail.com>
2025-11-03 20:28:52 +0330

    feat(mockups): add task status endpoint and add TaskResultView for Celery result tracking

--- mockups/views.py
+++ mockups/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from celery.result import AsyncResult
+from core.celery import app
 from .models import Mockup
 from .serializers import MockupSerializer
 from .tasks import generate_mockup_images_task
@@ -15,13 +17,43 @@ class CreateMockupView(APIView):
         if ser_data.is_valid():
             mockup = ser_data.save()
             
-            task = generate_mockup_images_task.delay(mockup.task_id)
+            task = generate_mockup_images_task.delay(mockup.id)
+            task_status = AsyncResult(task.id, app=app).status
 
             return Response({
-                "mockup_task_id": str(mockup.task_id),
-                "celery_task_id":str(task.id),
-                "status": "PENDING",
+                "task_id":str(task.id),
+                "status": task_status,
                 "message": "generating image started"
             }, status=status.HTTP_201_CREATED)
         
         return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskResultView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, task_id):
+        task_result = AsyncResult(task_id, app=app)
+        task_status = task_result.status
+
+        response_data = {
+            "task_id": str(task_id),
+            "status": task_status,
+            "results": [],
+            "error": None,
+        }
+
+        if not task_result.ready():
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        if task_status == "SUCCESS":
+            task_data = task_result.result
+            response_data["results"] = task_data["results"]
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        if task_status == "FAILURE":
+            response_data["error"] = str(task_result.result)
+            return Response(response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
